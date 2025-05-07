d0d9867f38f6636d1d2e63cdce8279bbe7cc6855
Jernej K <jernej.klancic@leanix.net>
2025-05-07 09:38:31 +0200

    Add caching mechanism

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const xml2js = require('xml2js');
+const NodeCache = require('node-cache');
 
 // setup express service
 const app = express();
 // Use the port provided by the host or default to 3000
 const port = process.env.PORT || 3000;
+// Cache items for 15 minutes (900 seconds)
+const cache = new NodeCache({ stdTTL: 900 });
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
@@ -28,16 +31,22 @@ const fetchFeed = async (feedUrl) => {
     }
 };
 
-// Define the API endpoint
+// Retrieve feeds for user
 app.get('/api/feed/:name', async (req, res) => {
     const mediumName = req.params.name;
     // Default to 5 items if 'size' is not specified
     const size = parseInt(req.query.size) || 5;
 
     const feedUrl = `https://medium.com/feed/@${mediumName}`;
+    console.log(`Fetching feeds: [${mediumName}, ${feedUrl}]`);
 
-    console.info(`name is: ${mediumName}`);
-    console.info(`feedUrl is: ${feedUrl}`);
+    const cacheKey = `${mediumName}:${size}`;
+    const cachedData = cache.get(cacheKey);
+
+    if (cachedData) {
+        console.log('Serving from cache');
+        return res.json(cachedData);
+    }
 
     try {
         const items = await fetchFeed(feedUrl);
@@ -49,6 +58,9 @@ app.get('/api/feed/:name', async (req, res) => {
             categories: item.category ? item.category.map(cat => cat._ || cat) : [],
             content: item['content:encoded']?.[0] || null
         }));
+
+        // Cache the formatted items
+        cache.set(cacheKey, limitedItems);
 
         res.json(limitedItems); // Send the formatted items as JSON response
     } catch (error) {
